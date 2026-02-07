bda414b870187cb3fed275e49657a085973eadbb
Dennis <191393713+Doct0rDan@users.noreply.github.com>
2026-02-07 22:53:20 +0100

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿Helper.Intro();
+
+
+Player player = new(0,0);
+WorldGrid grid = new WorldGrid(player);
+
+grid.GameUpdate(player);
+
+
+
+
+
---
+++ Rooms/FountainRoom.cs
@@ -0,0 +1,59 @@
+public partial class FountainRoom : EventRoom
+{
+
+    public bool IsActivated { get; private set; }
+    public FountainRoom(WorldGrid grid, int row, int column) : base(grid, row,column, RoomContents.Fountain)
+    {
+        //Console.WriteLine("FountainRoom created at:" + Position);
+    }
+
+    public override void OnEnter(Player player)
+    {
+        base.OnEnter();
+    }
+
+    public override void OnInteract()
+    {
+        IsActivated = true;
+        Console.WriteLine();
+        Console.WriteLine("+++++++++++++++++++++++");
+        Console.WriteLine("++Fountain activated!++");
+        Console.WriteLine("+++++++++++++++++++++++");
+        Console.WriteLine();
+    }
+
+    public void FountainSound(Player player)
+    {
+        if (player.Position == Position)
+        {
+            Console.WriteLine("There it is! The magnigicient Fountain");
+        }
+        if(!IsActivated && player.Position != Position)
+        {
+            string dir = "";
+
+            if(player.Position.X > Row)
+            {
+                dir += "N";
+            }
+            else if (player.Position.X < Row)
+            {
+                dir += "S";
+            }
+
+
+            if(player.Position.Y > Column)
+            {
+                dir += "W";
+            }
+            else if (player.Position.Y < Column)
+            {
+                dir += "E";
+            }
+
+            Console.WriteLine("YOU HEAR A DRIPPING COMING FROM " + dir + " !!"
+            + "THAT MUST BE THE FOUNTAIN!! ");
+        }
+        
+    }
+}
